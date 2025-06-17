10ba0f5c43ee3f75dbe82c173b3cc07633424c4b
Gooraeng <birdyoon1998@gmail.com>
2025-06-17 15:25:06 +0900

    work

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.mysite.sbb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Optional<Question> findBySubject(String s);
 
     Optional<Question> findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subjectLike);
 }
